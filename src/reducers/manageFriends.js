c7fc35829ab02ac087ff0cca87c090bd70f1d948
github username <chao.alexander.123@gmail.com>
2019-01-04 12:11:57 -0500

    create reducers to manage state of friends

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {friends: [...state.friends, action.friend]}
+    case "REMOVE_FRIEND":
+      return {friends: removeFriend(state, action)}
+    default:
+      return state
+  }
+}
+
+function removeFriend(state, action) {
+  return state.friends.filter(friend => friend.id !== action.id)
 }
