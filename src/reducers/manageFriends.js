a274080ddca40b62d04fca2656134b79ed281760
Gianna De La Cruz <gianna.jolibeth15@gmail.com>
2019-05-14 18:34:09 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return ( {
+        friends: [...state.friends, action.friend]
+      })
+    case 'REMOVE_FRIEND': 
+      let noNewFriends = state.friends.filter((friend) => {
+        return friend.id !== action.id
+      })
+      return ({
+        friends: noNewFriends
+      })
+    default:
+      return state
+  }
 }
